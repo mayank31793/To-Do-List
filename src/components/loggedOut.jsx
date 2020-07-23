418b825435b22fdf26ca992a48a2a016ef1a9464
Mayank <mayank31793@gmail.com>
2020-07-23 11:50:30 +0530

    changes navbar

--- src/components/loggedOut.jsx
+++ src/components/loggedOut.jsx
@@ -3,6 +3,8 @@ import React, { useState } from 'react';
 import GoogleLogin from 'react-google-login';
 import FacebookLogin from 'react-facebook-login';
 
+import styles from '../assets/styles/loggedOut.module.scss';
+
 const LoggedOut = (props) => {
     const responseAuth = (response) => {
         if('graphDomain' in response){
@@ -21,10 +23,39 @@ const LoggedOut = (props) => {
     }
 
     return ( 
-        <div>
-            <p>this is Logged Out</p>
-            <button onClick={props.handleLoginStatus}>Login</button>
-                                <GoogleLogin 
+        <div className={styles.container}>
+            <div className={styles.main_container}>
+                <div className={styles.image_container}>
+                    <div className={styles.image_div}>
+                        <img src="https://vil.xlri.ac.in/emdp/assets/images/login.svg" alt="login image" />
+                    </div>
+                </div>
+                <div className={styles.loginform_container}>
+                    <div className={styles.login_form}>
+                        <p className={styles.paragraph}>SignUp / SignIn with</p>
+                        <div className={styles.google_login}>
+                            <GoogleLogin 
+                                clientId="146469123782-kqf593qh32a6k7t5dq667miunu5sek33.apps.googleusercontent.com"
+                                buttonText="Login With Google"
+                                onSuccess={responseAuth}  
+                                onFailure={responseAuth}
+                                cookiePolicy={'single_host_origin'}
+                            />
+                        </div>
+                        <p className={styles.paragraph}>---- OR ----</p>
+                        <div className={styles.facebook_login}>
+                            <FacebookLogin
+                                appId="706223930200877"
+                                autoLoad={true}
+                                fields="name,email,picture"
+                                onClick={componentClicked}
+                                callback={responseAuth} 
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>            
+                                {/* <GoogleLogin 
                                     clientId="146469123782-kqf593qh32a6k7t5dq667miunu5sek33.apps.googleusercontent.com"
                                     buttonText="Login With Google"
                                     onSuccess={responseAuth}  
@@ -37,7 +68,7 @@ const LoggedOut = (props) => {
                                     fields="name,email,picture"
                                     onClick={componentClicked}
                                     callback={responseAuth} 
-                                />
+                                /> */}
         </div>
     );
 }
